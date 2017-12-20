0ea419fc63f3633f58b672f427ac9abaa6ca0f11
lucas nico zamora jabre <lzamora@alumnos.inf.utfsm.cl>
2017-12-20 00:43:44 -0300

    Migas de pan y Ficha listos

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import {Route, Switch} from 'react-router-dom';
 import {
     BrowserRouter as Router,
 } from 'react-router-dom'
+
 import Breadcrumbs  from 'react-router-dynamic-breadcrumbs';
+
 import NavBar from './NavBar';
 import Login from './Login';
 import Welcome from './Welcome';
@@ -14,18 +16,23 @@ import './App.css'
 const routes = {
     '/': 'Home',
     '/welcome': 'Welcome',
-    '/welcome/Ficha': 'Ficha Personal'
+    '/welcome/ficha': 'Ficha Personal'
 };
 
 class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <switch>
-                    <Route exact path="/" component={Login}/>
-                    <Route exact path="/welcome" component={Welcome}/>
-                    <Route exact path="/welcome/ficha" component={Ficha}/>
-                </switch>
+                <Router>
+                    <div>
+                        <Breadcrumbs mappedRoutes={routes} />
+                        <switch>
+                            <Route exact path="/" component={Login}/>
+                            <Route exact path="/welcome" component={Welcome}/>
+                            <Route exact path="/welcome/ficha" component={Ficha}/>
+                        </switch>
+                    </div>
+                </Router>
             </div>
 
         );
--- src/Ficha.js
+++ src/Ficha.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './Ficha.css'
 
 class Ficha extends React.Component {
 
     render () {
         return (
             <div>
-                <h1> Holi</h1>
+                <div className="col-sm-6 col-sm-offset-3">
+                <div className="panel panel-info">
+                    <div className="panel-heading"><h3>Ficha Personal</h3></div>
+                        <div className="panel-body">
+                            <div className="col-6 col-lg-4">
+                                <img src={require('./User.png')} />
+                            </div>
+                            <div className="col-12 col-sm-6 col-lg-8">
+                                <h5>Nombre</h5>
+                                <hr></hr>
+                                <h5>Carrera</h5>
+                                <hr></hr>
+                                <h5>Rol</h5>
+                                <hr></hr>
+                                <div className="col-sm-6 col-sm-offset-3">
+                                    <button type="button" class="btn btn-danger">Atrás</button>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+                </div>
             </div>
         );
     }
